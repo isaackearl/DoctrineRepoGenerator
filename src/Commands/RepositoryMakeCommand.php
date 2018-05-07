1d62150fcd20664b8b178343611202ef932dfb02
Isaac Earl <isaackearl@gmail.com>
2018-05-07 16:04:35 -0400

    add interface and repo

--- src/Commands/RepositoryMakeCommand.php
+++ src/Commands/RepositoryMakeCommand.php
@@ -9,9 +9,12 @@
 namespace IsaacKenEarl\DoctrineRepoGenerator\Commands;
 
 
-use Illuminate\Console\GeneratorCommand;
+use Illuminate\Console\Command;
+use Illuminate\Container\Container;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Composer;
 
-class RepositoryMakeCommand extends GeneratorCommand
+class RepositoryMakeCommand extends Command
 {
     /**
      * The name and signature of the console command.
@@ -19,13 +22,6 @@ class RepositoryMakeCommand extends GeneratorCommand
      * @var string
      */
     protected $signature = 'doctrine:make:repository';
-
-    /**
-     * The type of class being generated.
-     *
-     * @var string
-     */
-    protected $type = 'Repository';
 
     /**
      * The console command description.
@@ -38,6 +34,34 @@ class RepositoryMakeCommand extends GeneratorCommand
      * @var string
      */
     private $repoName;
+
+    /**
+     * @var Composer
+     */
+    private $composer;
+
+    /**
+     * @var Filesystem
+     */
+    private $files;
+
+    /**
+     * @var string
+     */
+    private $type = 'Repository';
+
+    /**
+     * Create a new command instance.
+     *
+     * @param Filesystem $files
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+        $this->composer = app()['composer'];
+    }
 
     /**
      * Alias for the fire method.
@@ -58,8 +82,10 @@ class RepositoryMakeCommand extends GeneratorCommand
     public function fire()
     {
         $this->repoName = $this->argument('name');
-        $this->makeRepository();
-        $this->generateServiceProvider();
+        $this->makeRepositoryInterface();
+        $this->makeDoctrineRepository();
+//        $this->makeRepository();
+//        $this->generateServiceProvider();
     }
 
     /**
@@ -70,5 +96,69 @@ class RepositoryMakeCommand extends GeneratorCommand
     protected function getStub()
     {
         // TODO: Implement getStub() method.
+    }
+
+    /**
+     * Generate the desired repository.
+     */
+    protected function makeRepositoryInterface()
+    {
+        $name = $this->argument('name');
+        if ($this->files->exists($path = $this->getInterfacePath($name))) {
+            $this->error($this->type . ' already exists!');
+            return;
+        }
+        $this->makeDirectory($path);
+        $this->files->put($path, $this->compileInterfaceStub());
+        $this->info('Repository created successfully.');
+        $this->composer->dumpAutoloads();
+    }
+
+    /**
+     * Compile the migration stub.
+     *
+     * @return string
+     */
+    protected function compileMigrationStub()
+    {
+        $stub = $this->files->get(__DIR__ . '/../stubs/migration.stub');
+        $this->replaceClassName($stub)
+            ->replaceSchema($stub)
+            ->replaceTableName($stub);
+        return $stub;
+    }
+
+    /**
+     * Get the path to where we should store the migration.
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function getInterfacePath($name)
+    {
+        return base_path() . '/app/Repositories/Interfaces' . $name . '.php';
+    }
+
+    /**
+     * Get the application namespace.
+     *
+     * @return string
+     */
+    protected function getAppNamespace()
+    {
+        return Container::getInstance()->getNamespace();
+    }
+
+    /**
+     * Build the directory for the class if necessary.
+     *
+     * @param  string $path
+     * @return string
+     */
+    protected function makeDirectory($path)
+    {
+        if (!$this->files->isDirectory(dirname($path))) {
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
+        }
     }
 }
